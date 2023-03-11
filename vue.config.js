a6f4db836331ccecc6d42d668c3c2b9b179871ff
Alejandro <alejandro@Alejandros-MacBook-Pro.local>
2023-03-11 01:34:58 +0100

    update url deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  publicPath:  process.env.NODE_ENV === "production" ? "/vue-examples/" : "",
+  publicPath:  process.env.NODE_ENV === "production" ? "/vue-examples" : "",
   transpileDependencies: true,
 });
